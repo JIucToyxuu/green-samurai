7c13e53a0250c41a270311599136aa61becb7dc1
jiuctoyxuu <antonduzhnov.90@gmail.com>
2014-12-18 15:20:14 +0400

    change paths

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -29,6 +29,38 @@ module PagesHelper
 		html.html_safe
 	end
 
+	def aside_menu
+		html = '<div class="col-sm-3 col-sm-height col-top main_sidebar"><aside><ul class="side_menu">'
+
+		html += '<li' + current(kak_eto_rabotaet_path) + '>'
+		html +=	link_to('как это работает', kak_eto_rabotaet_path)
+		html += '</li>'
+
+		html += '<li' + current(faq_path) + '>'
+		html +=	link_to('Частые вопросы', faq_path)
+		html += '</li>'
+
+		html += '<li' + current(kurs_uch_plan_path) + '>'
+		html +=	link_to('преподавателям', kurs_uspevaemost_path)
+		html += '</li>'
+
+		html += '<li' + current(about_path) + '>'
+		html +=	link_to('О проекте', about_path)
+		html += '</li>'
+
+		html += '<li' + current(kurs_uch_plan_path) + '>'
+		html +=	link_to('Новости', kurs_uspevaemost_path)
+		html += '</li>'
+
+		html += '<li' + current(contacts_path) + '>'
+		html +=	link_to('Контакты', contacts_path)
+		html += '</li>'
+
+		html += '</div></aside></ul>'
+
+		html.html_safe
+	end
+
 	def current(path)
 		if current_page?(path)
 			return ' class="current" '
