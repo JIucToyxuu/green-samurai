c786d86d5f082c214e714ada68ce6e0cbc6eb841
jiuctoyxuu <antonduzhnov.90@gmail.com>
2014-12-23 11:00:09 +0400

    add MANDRILL - Heroku addon for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,11 +83,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
-     :address              => "smtp.gmail.com",
+     :address              => "smtp.mandrillapp.com",
      :port                 => 587,
-     :domain               => 'heroku.com',
-     :user_name            => 'duzhnovwork@gmail.com',
-     :password             => 'Vinchester1',
+     :user_name            => ENV['MANDRILL_USERNAME'],
+     :password             => ENV['MANDRILL_APIKEY'];
      :authentication       => :plain
   }
 end
