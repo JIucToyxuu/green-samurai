8c3a31f61af21bab9b5152e6c8cc36d941bc8d3d
jiuctoyxuu <antonduzhnov.90@gmail.com>
2014-12-24 16:49:49 +0400

    add worked login with facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,14 +2,14 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(provider)
     class_eval %Q{
       def #{provider}
-        @user = User.find_for_oauth(env["omniauth.auth"], current_user)
+        @user = User.find_for_oauth(env["omniauth.auth"])
 
         if @user.nil?
           redirect_to new_user_registration_url
         else
-          if @user.persisted?
+          
             sign_in_and_redirect @user, event: :authentication
-          end
+          
         end
 
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  # has_many :identitys, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,19 +7,13 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true
 
-  def self.find_for_oauth(auth, signed_in_resource = nil)
+  def self.find_for_oauth(auth)
 
     identity = Identity.find_for_oauth(auth)
 
-    user = signed_in_resource ? signed_in_resource : identity.user
+    user = User.where(:email => auth.info.email).first if auth.info.email
 
-    if user.nil?
-       email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-       email = auth.info.email if email_is_verified
-       user = User.where(:email => email).first if email
-    end
-
-    if identity.user != user
+    if !user.nil?
       identity.user = user
       identity.save!
     end
