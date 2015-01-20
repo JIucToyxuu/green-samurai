e23bc876350058b51703d76bbc515d8dff1cf9a0
jiuctoyxuu <antonduzhnov.90@gmail.com>
2015-01-20 18:11:36 +0400

    fix problem with regex

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,7 +2,7 @@ class FeedbacksController < ApplicationController
 	def create
 
 		if feedback_params[:title].nil? || feedback_params[:title].empty?
-			params[:feedback][:title] = feedback_params[:message][0..(feedback_params[:message] =~ /\r/)-2 || feedback_params[:message].length]
+			params[:feedback][:title] = feedback_params[:message][0..(feedback_params[:message] =~ /\r/) || feedback_params[:message].length]
 		end
 		puts 'params'
 		puts params
