d0b284a2f426ed342777b618f121326fc6aaaeb3
jiuctoyxuu <antonduzhnov.90@gmail.com>
2015-01-22 11:54:21 +0300

    fix problem with lower case for pg database

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,10 @@ class PagesController < ApplicationController
 		end
 		if params[:q].nil?
 			if current_user.nil?
-				@courses = Course.all.order(params[:sort_by])
+				@courses = Course.all.order("LOWER(#{params[:sort_by]})")
 			else
-				@courses = Course.all.joins(:pupils).where.not('pupils.users_id' => current_user.id).order(params[:sort_by])
-				@all = Course.all.order(params[:sort_by])
+				@courses = Course.all.joins(:pupils).where.not('pupils.users_id' => current_user.id).order("LOWER(#{params[:sort_by]})")
+				@all = Course.all.order("LOWER(#{params[:sort_by]})")
 				@all.each do |course|
 					if !course.pupils[0]
 						@courses << course
@@ -18,14 +18,14 @@ class PagesController < ApplicationController
 				end
 			end
 		else
-			@courses = Course.where("lower(name) LIKE '%#{params[:q].downcase}%'")
+			@courses = Course.where("LOWER(name) LIKE '%#{params[:q].downcase}%'")
 		end
 		current_user_courses
 
 		#news for slide
 		@news = News.last(2)
 		#category for sorting data
-		@category = [["По дате начала курса", 'date_begin'], ["По теме", 'LOWER(name)'], ["По университету", 'university_id'], ["В свободном доступе", 'free']]
+		@category = [["По дате начала курса", 'date_begin'], ["По теме", 'name'], ["По университету", 'university_id'], ["В свободном доступе", 'free']]
 	end
 
 	def courses
@@ -34,9 +34,9 @@ class PagesController < ApplicationController
 		end
 		@universities = University.includes(:courses)
 		if !params[:q].nil?
-			@universities = University.joins(:courses).includes(:courses).where("lower('courses'.'name') LIKE '%#{params[:q].downcase}%'")
+			@universities = University.joins(:courses).includes(:courses).where("LOWER('courses'.'name') LIKE '%#{params[:q].downcase}%'")
 		end
-		@universities = @universities.all.joins(:courses).order('universities.id').includes(:courses).order("lower(courses.#{params[:sort_by]})")
+		@universities = @universities.all.joins(:courses).order('universities.id').includes(:courses).order("LOWER(courses.#{params[:sort_by]})")
 		#category for sorting data
 		@category = [["По дате начала курса", 'date_begin'], ["По теме", 'name'], ["В свободном доступе", 'free']]
 		current_user_courses
@@ -75,10 +75,10 @@ class PagesController < ApplicationController
 			params[:sort_by] = 'date_begin'
 		end
 		#category for sorting data
-		@category = [["По дате начала курса", 'date_begin'], ["По теме", 'LOWER(name)'], ["В свободном доступе", 'free']]
+		@category = [["По дате начала курса", 'date_begin'], ["По теме", 'name'], ["В свободном доступе", 'free']]
 
 		@university = University.find_by_abbreviation(params[:university])
-		@courses =  @university.courses.order(params[:sort_by]);
+		@courses =  @university.courses.order("LOWER(#{params[:sort_by]})");
 		current_user_courses
 	end
 
